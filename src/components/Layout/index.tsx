3052981a2e1ecd964a6911fcc959cc3f57d6fd1e
mik284 <mikeykamau222@gmail.com>
2024-07-25 18:05:33 +0300

    feat: update calendar and layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from '@umijs/max';
 import { ConfigProvider } from 'antd';
+import Navbar from '../Navbar';
 
 function Layout() {
   return (
@@ -14,6 +15,7 @@ function Layout() {
       >
         <div className="grid content-between mx-auto h-screen min-h-screen px-4 w-full 2xl:max-w-[1440px] scroll-smooth">
           <div className="grid grid-cols-1">
+            <Navbar/>
             <Outlet />
           </div>
         </div>
---
+++ src/pages/Calendar/index.tsx
@@ -0,0 +1,41 @@
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.scss';
+import 'react-big-calendar/lib/sass/styles.scss';
+
+import moment from 'moment';
+
+const localizer = momentLocalizer(moment);
+
+const CalendarPage: React.FC = () => {
+  const myEventsList = [
+    {
+      title: 'Meeting',
+      start: new Date(2024, 6, 23, 10, 0), // year, month (0-indexed), day, hour, minute
+      end: new Date(2024, 6, 23, 12, 0), // year, month (0-indexed), day, hour, minute
+    },
+    {
+      title: 'Lunch Break',
+      start: new Date(2024, 6, 24, 12, 0),
+      end: new Date(2024, 6, 24, 13, 0),
+    },
+    {
+      title: 'Conference',
+      start: new Date(2024, 6, 25, 9, 0),
+      end: new Date(2024, 6, 25, 17, 0),
+    },
+  ];
+  return (
+    <div className="max-h-svh h-screen mt-4">
+      <section className="h-inherit">
+        <Calendar
+          localizer={localizer}
+          events={myEventsList}
+          startAccessor="start"
+          endAccessor="end"
+        />
+      </section>
+    </div>
+  );
+};
+
+export default CalendarPage;
