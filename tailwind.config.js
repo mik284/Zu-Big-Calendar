c14ad78cff2f3fdc588ae8721ca47a2821a56243
mik284 <mikeykamau222@gmail.com>
2024-07-25 18:03:40 +0300

    feat: add tailwind plug and calendar pack

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      height: {
+        inherit: 'inherit',
+      },
+    },
+  },
+  content: [
+    './src/pages/**/*.tsx',
+    './src/components/**/*.tsx',
+    './src/layouts/**/*.tsx',
+  ],
+};
